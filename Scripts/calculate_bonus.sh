474e3616a3c218bade2989fb91aad62dfbc5294a
Franklin Edward <franklinraja11@gmail.com>
2023-01-17 15:20:35 +0530

    Day 3 updated

--- Scripts/calculate_bonus.sh
+++ Scripts/calculate_bonus.sh
@@ -4,7 +4,7 @@ DATE=$(date +%Y-%m-%dT%T)
 INPUT_FILE_PATH="./readinputs/"
 
 CUTOFF_DATE="01-Jan-2023"
-fixed_bonus=10000
+FIXED_BONUS=10000
 
 FILE=$(cat "$INPUT_FILE_PATH"input.txt | sed '1d')
 
@@ -13,33 +13,36 @@ for line in ${FILE[@]}
 do
 	NAME=$(echo $line | awk '{print $2}')
 	EMP_TYPE=$(echo $line | awk '{print $3}')
-	joining_date=$(echo $line | awk '{print $4}')
-	status=$(echo $line | awk '{print $5}')
+	JOINING_DATE=$(echo $line | awk '{print $4}')
+	STATUS=$(echo $line | awk '{print $5}')
 
-	diff=$(echo $(($(date -jf "%d-%b-%Y" $CUTOFF_DATE +%s) - $(date -jf "%d-%b-%Y" $joining_date +%s))))
+	DIFF=$(echo $(($(date -jf "%d-%b-%Y" $CUTOFF_DATE +%s) - $(date -jf "%d-%b-%Y" $JOINING_DATE +%s))))
 
-	DIFF_YEARS=$(expr $diff / 31536000)
+	DIFF_YEARS=$(expr $DIFF / 31536000)
 
-	if [[ $DIFF_YEARS  -lt 1 ]] && [[ $status == "Active" ]]
+	if [[ $DIFF_YEARS  -lt 1 ]] && [[ $STATUS == "Active" ]]
 	then
-		DIFF_MONTHS=$(expr $diff / 2592000)
-		BONUS=$(echo "$fixed_bonus*$DIFF_MONTHS/12" | bc)
+		DIFF_MONTHS=$(expr $DIFF / 2592000)
+		BONUS=$(echo "$FIXED_BONUS*$DIFF_MONTHS/12" | bc)
 	elif [[ $DIFF_YEARS -ge 1 ]]
 	then
-		BONUS=$(echo "$fixed_bonus*$DIFF_YEARS" | bc)
+		BONUS=$(echo "$FIXED_BONUS*$DIFF_YEARS" | bc)
 	else
 		BONUS=0
 	fi
 
-  if [[ $status == "InActive" ]]
+  if [[ $STATUS == "InActive" ]]
   then
   	BONUS=0
   fi
 
-  if [[ $BONUS -eq 0 ]]
-  then
-  	echo "$NAME joined as $emp_type employee, joined on $joining_date. No Bonus" >> output-"$DATE".bonus
-  else
-  	echo "$NAME joined as $emp_type employee, joined on $joining_date. Bonus has to be paid Rs $BONUS" >> output-"$DATE".bonus
-  fi
+  line="$NAME $EMP_TYPE $JOINING_DATE $STATUS $BONUS" 
+  echo $line >> updatedinput.txt
 done 
+
+sort -k5,5nr updatedinput.txt > sort.txt``
+
+awk '{if ($5 == 0) print $1 " is joined as " tolower($2) ", joined on " $3 ". No Bonus" > "output-'$DATE'.bonus"; else print $1 " is joined as " tolower($2) ", joined on " $3 ". Bonus has to be paid Rs." $5 > "output-'$DATE'.bonus"}' sort.txt
+
+rm sort.txt
+rm updatedinput.txt
